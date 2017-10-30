dc3983036b14b112e515cee58bc1859c6bdf7394
tianliangliang <tianliangliang@mobike.com>
2017-10-30 23:46:40 +0800

    init

---
+++ src/main/java/com/tll/sbcl/springbcl/SpringBclApplication.java
@@ -0,0 +1,12 @@
+package com.tll.sbcl.springbcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBclApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tll/sbcl/springbcl/SpringBclApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tll.sbcl.springbcl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBclApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
