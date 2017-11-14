8e6d3e49e35dc6170e2b656eb5756f74de1c8bc6
tianliangliang <tianliangliang@mobike.com>
2017-11-14 13:23:49 +0800

    learn redis

--- springboot-learn/src/test/java/com/tll/sbcl/springbootlearn/chapter4/chapter4Tests.java
+++ springboot-learn/src/test/java/com/tll/sbcl/springbootlearn/chapter4/chapter4Tests.java
@@ -1,18 +1,15 @@
 package com.tll.sbcl.springbootlearn.chapter4;
 
-import com.tll.sbcl.springbootlearn.chapter4.springDataJps.User;
-import com.tll.sbcl.springbootlearn.chapter4.springDataJps.UserRepository;
+import com.tll.sbcl.springbootlearn.chapter4.redis.User;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.List;
 
 /**
  * Created by tll on 08/11/2017 12:56
@@ -80,27 +77,53 @@ public class chapter4Tests {
 //        Assert.assertEquals(9, userRepository.findAll().size());
 //    }
 
+//    @Autowired
+//    @Qualifier("primaryJdbcTemplate")
+//    protected JdbcTemplate jdbcTemplate1;
+//    @Autowired
+//    @Qualifier("secondaryJdbcTemplate")
+//    protected JdbcTemplate jdbcTemplate2;
+//    @Before
+//    public void setUp() {
+//        jdbcTemplate1.update("DELETE  FROM  USER ");
+//        jdbcTemplate2.update("DELETE  FROM  USER ");
+//    }
+//    @Test
+//    public void test() throws Exception {
+//        // 往第一个数据源中插入两条数据
+//        jdbcTemplate1.update("insert into user(id,name,age) values(?, ?, ?)", 1, "aaa", 20);
+//        jdbcTemplate1.update("insert into user(id,name,age) values(?, ?, ?)", 2, "bbb", 30);
+//        // 往第二个数据源中插入一条数据，若插入的是第一个数据源，则会主键冲突报错
+//        jdbcTemplate2.update("insert into user(id,name,age) values(?, ?, ?)", 1, "aaa", 20);
+//        // 查一下第一个数据源中是否有两条数据，验证插入是否成功
+//        Assert.assertEquals("2", jdbcTemplate1.queryForObject("select count(1) from user", String.class));
+//        // 查一下第一个数据源中是否有两条数据，验证插入是否成功
+//        Assert.assertEquals("1", jdbcTemplate2.queryForObject("select count(1) from user", String.class));
+//    }
+
+
     @Autowired
-    @Qualifier("primaryJdbcTemplate")
-    protected JdbcTemplate jdbcTemplate1;
-    @Autowired
-    @Qualifier("secondaryJdbcTemplate")
-    protected JdbcTemplate jdbcTemplate2;
-    @Before
-    public void setUp() {
-        jdbcTemplate1.update("DELETE  FROM  USER ");
-        jdbcTemplate2.update("DELETE  FROM  USER ");
+    private StringRedisTemplate stringRedisTemplate;
+    @Test
+    public void test1() throws Exception {
+        // 保存字符串
+        stringRedisTemplate.opsForValue().set("aaa", "111");
+        Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
     }
+
+    @Autowired
+    private RedisTemplate<String, User> redisTemplate;
     @Test
     public void test() throws Exception {
-        // 往第一个数据源中插入两条数据
-        jdbcTemplate1.update("insert into user(id,name,age) values(?, ?, ?)", 1, "aaa", 20);
-        jdbcTemplate1.update("insert into user(id,name,age) values(?, ?, ?)", 2, "bbb", 30);
-        // 往第二个数据源中插入一条数据，若插入的是第一个数据源，则会主键冲突报错
-        jdbcTemplate2.update("insert into user(id,name,age) values(?, ?, ?)", 1, "aaa", 20);
-        // 查一下第一个数据源中是否有两条数据，验证插入是否成功
-        Assert.assertEquals("2", jdbcTemplate1.queryForObject("select count(1) from user", String.class));
-        // 查一下第一个数据源中是否有两条数据，验证插入是否成功
-        Assert.assertEquals("1", jdbcTemplate2.queryForObject("select count(1) from user", String.class));
+        // 保存对象
+        User user = new User("超人", 20);
+        redisTemplate.opsForValue().set(user.getUsername(), user);
+        user = new User("蝙蝠侠", 30);
+        redisTemplate.opsForValue().set(user.getUsername(), user);
+        user = new User("蜘蛛侠", 40);
+        redisTemplate.opsForValue().set(user.getUsername(), user);
+        Assert.assertEquals(20, redisTemplate.opsForValue().get("超人").getAge().longValue());
+        Assert.assertEquals(30, redisTemplate.opsForValue().get("蝙蝠侠").getAge().longValue());
+        Assert.assertEquals(40, redisTemplate.opsForValue().get("蜘蛛侠").getAge().longValue());
     }
 }
